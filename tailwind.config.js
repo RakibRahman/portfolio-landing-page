0170c2a658ab470b8c49c550e3bcc4d3fbe7b975
RakibRahman <md.rr.talukder@gmail.com>
2021-02-04 23:12:01 +0600

    Top Part & Nav Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: "#f14e0d",
+      },
+      backgroundImage: (theme) => ({
+        "hero-circle": "url('/img/circle.png')",
+      }),
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
